e248e114148d017cfef2cd25985d0a9d8501d155
phonghahaa <nguyenthienphong1401@gmail.com>
2026-09-16 16:16:48 +0700

    Add files via upload

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by MAY 05 on 14/9/26.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
